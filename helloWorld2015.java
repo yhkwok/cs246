6bd12114e79b93d728bc0da5678cef589b454773
yhkwok <kwo13001@byui.edu>
2015-05-13 19:17:11 -0600

    Hello Github World Test..

--- helloWorld2015.java
+++ helloWorld2015.java
@@ -16,7 +16,7 @@ public class Helloworld {
      */
     public static void main(String[] args) {
         System.out.println("Hello Github World!\n");
-		System.out.println("Testing....\n");
+		System.out.println("Testing.....\n");
     }
     
 }
