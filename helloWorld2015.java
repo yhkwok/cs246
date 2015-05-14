0cc0d9f5852380167a27161abc3621f501b9c36f
yhkwok <kwo13001@byui.edu>
2015-05-13 19:05:52 -0600

    Hello Github World

--- helloWorld2015.java
+++ helloWorld2015.java
@@ -15,7 +15,7 @@ public class Helloworld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!\n");
+        System.out.println("Hello Github World!\n");
     }
     
 }
